1b78743039e4ba1f9b56402e7b74c8af3ffa1a9b
Jason McNeill <vrtjason@gmail.com>
2026-06-27 16:36:57 -0700

    Change output

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3003;
 
-app.get("/", (req, res) => res.send("PhoenixRCW.org is alive!"));
+app.get("/", (req, res) => res.send("PhoenixRCW.org is live!"));
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
