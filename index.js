473ded1b0581997d31823fde8b481d85781a2621
Jason McNeill <vrtjason@gmail.com>
2026-06-27 16:37:39 -0700

    Test

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3003;
 
-app.get("/", (req, res) => res.send("PhoenixRCW.org is live!"));
+app.get("/", (req, res) => res.send("PhoenixRCW.org is live and kicking!"));
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
