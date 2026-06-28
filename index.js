b9aa70d064f750129c29f486113af11985c8d6b4
Jason McNeill <vrtjason@gmail.com>
2026-06-27 18:34:55 -0700

    Use a public directory

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = 3003;
 
-app.get("/", (req, res) => res.send("PhoenixRCW.org is live!"));
+app.set("view engine", "ejs");
+app.set("views", "./app/views");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public"), { maxAge: "1d" }));
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
