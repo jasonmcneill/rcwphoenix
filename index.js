6f28180a471bdd18aae5af5f85c151d3f9f1e48e
Jason McNeill <vrtjason@gmail.com>
2026-06-28 13:52:38 -0700

    Remove EJS for now

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require("express");
 const app = express();
 const port = 3003;
 
-app.set("view engine", "ejs");
-app.set("views", "./app/views");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public"), { maxAge: "1d" }));
